e346b64a485223a971a10ed7aa0002b9a0683127
Mike Schwörer <pubgit@mikescher.com>
2020-07-08 09:16:00 +0200

    Razor Page Model

--- Pages/Index2.cshtml.cs
+++ Pages/Index2.cshtml.cs
@@ -1,12 +1,15 @@
+using System;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace youtube_dl_viewer.Pages
 {
-    public class Index2 : PageModel
+    public class Index2Model : PageModel
     {
+        public string Message { get; private set; } = "PageModel in C#";
+
         public void OnGet()
         {
-            
+            Message += $" Server time is { DateTime.Now }";
         }
     }
 }
