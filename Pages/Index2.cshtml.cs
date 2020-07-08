4bfa367e2068c7cbff80399305796248921a6d4c
Mike Schwörer <pubgit@mikescher.com>
2020-07-08 09:15:08 +0200

    Razor Page

---
+++ Pages/Index2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace youtube_dl_viewer.Pages
+{
+    public class Index2 : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,7 @@ namespace youtube_dl_viewer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,6 +32,8 @@ namespace youtube_dl_viewer
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+                
+                endpoints.MapRazorPages(); // => localhost:5000/Index2
             });
         }
     }
