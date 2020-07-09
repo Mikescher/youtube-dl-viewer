43ae21169b262c8572b75f5cb4af0c7562499f19
Mike Schwörer <pubgit@mikescher.com>
2020-07-09 10:32:39 +0200

    css data view: [detailed]

--- staticfiles/script.js
+++ staticfiles/script.js
@@ -13,7 +13,14 @@ function formatSeconds(sec) {
 }
 
 function formatDate(date) {
-    return date.substr(0, 4) + '-' + date.substr(4, 2) + '-' + date.sub(6, 2);
+    return date.substr(0, 4) + '-' + date.substr(4, 2) + '-' + date.substr(6, 2);
+}
+
+function formatNumber(num) {
+    num += '';
+    let rex = /(\d+)(\d{3})/;
+    while (rex.test(num)) num = num.replace(rex, '$1' + '.' + '$2');
+    return num;
 }
 
 window.onload = function() { 
@@ -48,10 +55,10 @@ function initData()
         if (info.hasNonNull('fulltitle')) html += '<div class="title">' + escapeHtml(info['fulltitle']) + '</div>';
         else if (info.hasNonNull('title')) html += '<div class="title">' + escapeHtml(info['title']) + '</div>';
 
-        if (info.hasArrayWithValues('category')) 
+        if (info.hasArrayWithValues('categories')) 
         {
             html += '<div class="catlist">';
-            for (const c of info['category']) html += '<div class="category">' + escapeHtml(c) + '</div>';
+            for (const c of info['categories']) html += '<div class="category">' + escapeHtml(c) + '</div>';
             html += '</div>';
         }
         
@@ -68,37 +75,61 @@ function initData()
 
         if (info.hasNonNull('extractor')) html += '<div class="extractor">' + escapeHtml(info["extractor"]) + '</div>';
 
-        if (info.hasNonNull('uploader')) html += '<div class="uploader">' + escapeHtml(info["uploader"]) + '</div>';
+        if (info.hasNonNull('uploader')) 
+        {
+            if (info.hasNonNull('channel_url')) html += '<a href="' + escapeHtml(info["channel_url"]) + '" class="uploader">' + escapeHtml(info["uploader"]) + '</a>';
+            else if (info.hasNonNull('uploader_url')) html += '<a href="' + escapeHtml(info["uploader_url"]) + '" class="uploader">' + escapeHtml(info["uploader"]) + '</a>';
+            else html += '<div class="uploader">' + escapeHtml(info["uploader"]) + '</div>';
+                
+        }
 
         if (info.hasNonNull('like_count') && info.has('dislike_count')) 
         {
             html += '<div class="like_count">' + escapeHtml(info["like_count"]) + '</div>';
             html += '<div class="dislike_count">' + escapeHtml(info["dislike_count"]) + '</div>';
+
+            html += '<div class="likedislikebar">';
+            html += '  <div class="like_bar" style="width: ' + (100 * info["like_count"] / (info["like_count"] + info["dislike_count"])) + '%"><div class="like_bar_count">' + escapeHtml(info["like_count"]) + '</div></div>';
+            html += '  <div class="dislike_bar" style="width: ' + (100 * info["dislike_count"] / (info["like_count"] + info["dislike_count"])) + '%"><div class="dislike_bar_count">' + escapeHtml(info["dislike_count"]) + '</div></div>';
+            html += '</div>';
         }
 
-        if (info.hasNonNull('view_count')) html += '<div class="view_count">' + escapeHtml(info["view_count"]) + '</div>';
+        if (info.hasNonNull('view_count')) html += '<div class="view_count">' + escapeHtml(formatNumber(info["view_count"])) + '</div>';
 
         if (info.hasNonNull('width') && info.has('height')) html += '<div class="size">' + escapeHtml(info["width"]) + 'x' + escapeHtml(info["height"]) + '</div>';
 
-        if (info.hasNonNull('upload_date')) html += '<div class="view_count">' + escapeHtml(formatDate(info["upload_date"])) + '</div>';
+        if (info.hasNonNull('upload_date')) html += '<div class="upload_date">' + escapeHtml(formatDate(info["upload_date"])) + '</div>';
+
+        if (vid['data']['description'] !== null) html += '<div class="description">' + escapeHtml(vid['data']['description']) + '</div>';
+        else if (info.hasNonNull('description')) html += '<div class="description">' + escapeHtml(info['description']) + '</div>';
 
         html += '</div>';
         html += "\n\n";
     }
 
-    document.querySelector('main').innerHTML = html;
+    document.querySelector('#content').innerHTML = html;
 }
 
 function initButtons()
 {
     document.querySelector('.btn-display').addEventListener('click', () => 
     {
-        const main = document.querySelector('main');
+        const main = document.querySelector('#content');
         
              if (main.classList.contains('lstyle_detailed')) { main.classList.remove('lstyle_detailed'); main.classList.add('lstyle_grid');     }
         else if (main.classList.contains('lstyle_grid'))     { main.classList.remove('lstyle_grid');     main.classList.add('lstyle_compact');  }
         else if (main.classList.contains('lstyle_compact'))  { main.classList.remove('lstyle_compact');  main.classList.add('lstyle_tabular');  }
         else if (main.classList.contains('lstyle_tabular'))  { main.classList.remove('lstyle_tabular');  main.classList.add('lstyle_detailed'); }
+    });
+
+    document.querySelector('.btn-width').addEventListener('click', () =>
+    {
+        const main = document.querySelector('#content');
+
+             if (main.classList.contains('lstyle_width_medium')) { main.classList.remove('lstyle_width_medium'); main.classList.add('lstyle_width_wide');   }
+        else if (main.classList.contains('lstyle_width_wide'))   { main.classList.remove('lstyle_width_wide');   main.classList.add('lstyle_width_full');   }
+        else if (main.classList.contains('lstyle_width_full'))   { main.classList.remove('lstyle_width_full');   main.classList.add('lstyle_width_small');  }
+        else if (main.classList.contains('lstyle_width_small'))  { main.classList.remove('lstyle_width_small');  main.classList.add('lstyle_width_medium'); }
     });
 
     document.querySelector('.btn-order').addEventListener('click', () =>
