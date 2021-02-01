28754a560a259360f50052d31ac25813c25f72ba
Mike Schwörer <pubgit@mikescher.com>
2021-02-01 05:24:47 +0100

    fix wrong js url in /Jobs

--- MAKE_PUBLISH.bat
+++ MAKE_PUBLISH.bat
@@ -1,4 +1,7 @@
 
+rm releases/youtube-dl-viewer
+rm releases/youtube-dl-viewer.exe
+
 dotnet publish -c Release -r linux-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
 
 rm bin/Release/netcoreapp3.1/linux-x64/publish/appsettings.json
