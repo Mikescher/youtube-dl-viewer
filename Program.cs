9da656be241cd7e7dd91eb29fdfde5835dd00a55
Mike Schwörer <pubgit@mikescher.com>
2021-03-24 21:58:47 +0100

    version 0.36

--- Program.cs
+++ Program.cs
@@ -34,7 +34,7 @@ namespace youtube_dl_viewer
 
         public static Timer CronTimer;
         
-        public static string Version => "0.35";
+        public static string Version => "0.36";
 
         public static readonly Dictionary<int, DataDirData> DataCache = new Dictionary<int, DataDirData>();
 
