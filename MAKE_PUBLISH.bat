6745ff8c511ccd55a18615bd38bcd952cffcbb19
Mike Schwörer <pubgit@mikescher.com>
2021-09-16 19:42:57 +0200

    bugfix empty theme chooser

--- MAKE_PUBLISH.bat
+++ MAKE_PUBLISH.bat
@@ -4,6 +4,8 @@ dotnet clean
 rm releases/youtube-dl-viewer
 rm releases/youtube-dl-viewer.exe
 
+tsc
+
 dotnet publish -c Release -r linux-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
 
 rm bin/Release/netcoreapp3.1/linux-x64/publish/appsettings.json
